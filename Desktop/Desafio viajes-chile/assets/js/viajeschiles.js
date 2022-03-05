638e7cbe516b4f8626c40017b959f89479e96114
Osmaryz <osmigallecon@gmail.com>
2022-03-05 12:01:53 -0300

    adjunta cambios en index.html

--- Desktop/Desafio viajes-chile/assets/js/viajeschiles.js
+++ Desktop/Desafio viajes-chile/assets/js/viajeschiles.js
@@ -1,9 +1,5 @@
-/* 
-$( document ).ready(function() {
-    $(".nav-link").on("click", function (e) {
-      e.preventDefault();
-      const href = $(this).attr("href");
-      $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
-    });
+<script>
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover(); 
 });
- */
+</script>
